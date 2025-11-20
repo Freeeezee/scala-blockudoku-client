a6210721a593488163f066182dc177835535fe4c
dflox <f.dritter@htwg-konstanz.de>
2025-11-20 09:55:21 +0100

    feat: add settings modal with color scheme options and save/load functionality

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import './styles/main.less';
 import $ from 'jquery';
-import './scss/styles.scss';
 import {gridHtml} from "./views/grid-view";
 import {getGameState} from "./services/game.service";
 import 'bootstrap';
+import './scss/styles.scss';
+import './styles/main.less';
+import {initSettingsHtml} from "./views/settings.view";
 
 $('#grid-container').html(gridHtml());
 
 getGameState();
+initSettingsHtml();
--- src/services/settings.service.ts
+++ src/services/settings.service.ts
@@ -5,3 +5,9 @@ const API_URL = process.env.API_URL;
 export const downloadGameState = () => {
     $.get(`${API_URL}/download`);
 }
+
+
+export const setColor = (colorIndex: number) => {
+    // TODO: Exchange all tiles on the grid
+    navigator.sendBeacon(`${API_URL}/color/${colorIndex}`);
+}
