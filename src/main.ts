9841e12d51c41b9e8b59955b9b2c584ee7cd9a0b
Tom Berger <berger.tom@mail.de>
2025-12-11 15:34:14 +0100

    feat: analytics

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import {initializeBootstrapTooltips} from "./utils/bootstrap.util";
 import {initializeApp} from "./utils/app.util";
 
 export const main = async () => {
-    
-
     initializeApp();
 
     await handleQueryParams();
