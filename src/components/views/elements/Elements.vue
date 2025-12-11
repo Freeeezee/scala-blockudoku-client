73aa68acdc69650ec78428708c3a1ff93f98b261
dflox <f.dritter@htwg-konstanz.de>
2025-12-11 16:59:40 +0100

    feat: refactor Elements component to use v-for for dynamic rendering

--- src/components/views/elements/Elements.vue
+++ src/components/views/elements/Elements.vue
@@ -10,8 +10,11 @@ const elements = computed(() => app.gameState.value.elements);
 
 <template>
   <div v-if="elements" class="row g-0 justify-content-center">
-    <Element :index="0" :elements="elements"/>
-    <Element :index="1" :elements="elements"/>
-    <Element :index="2" :elements="elements"/>
+    <Element
+        v-for="(el, i) in elements"
+        :key="i"
+        :index="i"
+        :elements="elements"
+    />
   </div>
 </template>
