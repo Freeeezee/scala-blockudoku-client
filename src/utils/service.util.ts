e54eefc4792d67cc2c1d05ba280f8360566fa705
Tom Berger <berger.tom@mail.de>
2025-12-18 08:17:41 +0100

    fix: handle network errors

--- src/utils/service.util.ts
+++ src/utils/service.util.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-// Advantages:
-// - pre-define instance with base url
-// - modern promise-based API
-// - middleware-like global interceptors
-
 const API_URL = process.env.API_URL;
 
 const instance = axios.create({
@@ -14,21 +9,30 @@ const instance = axios.create({
 });
 
 export const get = async <T> (url: string): Promise<T | null> => {
-    const response = await instance.get<T>(url);
+    try {
+        const response = await instance.get<T>(url);
 
-    if (response.status !== 200) {
+        if (response.status !== 200) {
+            return null;
+        }
+
+        return response.data;
+    } catch (error) {
         return null;
     }
-
-    return response.data;
 }
 
 export const post = async <T> (url: string, data: any): Promise<T | null> => {
-    const response = await instance.post<T>(url, data);
+    try {
+        const response = await instance.post<T>(url, data);
 
-    if (response.status !== 200) {
+        if (response.status !== 200) {
+            return null;
+        }
+
+        return response.data;
+    }
+    catch (error) {
         return null;
     }
-
-    return response.data;
 }
